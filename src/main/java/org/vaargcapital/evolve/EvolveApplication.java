f20956ed58beb6d08c52727d7ecc8dcd77435134
comissarie <nkvett@icloud.com>
2026-01-30 12:57:48 +0300

    Initial commit

---
+++ src/main/java/org/vaargcapital/evolve/EvolveApplication.java
@@ -0,0 +1,13 @@
+package org.vaargcapital.evolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolveApplication.class, args);
+    }
+
+}
